ae766955ea1885669c8389e1072fdac20270be34
N. Niraula <subtlenv@gmail.com>
2020-01-07 12:13:39 +0545

    created location util which has quite useful methods. :fire:

---
+++ locationlibrary/src/main/java/np/com/naveenniraula/locationlibrary/util/LocationUtil.java
@@ -0,0 +1,59 @@
+package np.com.naveenniraula.locationlibrary.util;
+
+import android.location.Location;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class LocationUtil {
+
+    public static Location getLocationInLatLngRad(double radiusInMeters, Location currentLocation) {
+        double x0 = currentLocation.getLongitude();
+        double y0 = currentLocation.getLatitude();
+
+        Random random = new Random();
+
+        // Convert radius from meters to degrees.
+        double radiusInDegrees = radiusInMeters / 111320f;
+
+        // Get a random distance and a random angle.
+        double u = random.nextDouble();
+        double v = random.nextDouble();
+        double w = radiusInDegrees * Math.sqrt(u);
+        double t = 2 * Math.PI * v;
+        // Get the x and y delta values.
+        double x = w * Math.cos(t);
+        double y = w * Math.sin(t);
+
+        // Compensate the x value.
+        double new_x = x / Math.cos(Math.toRadians(y0));
+
+        double foundLatitude;
+        double foundLongitude;
+
+        foundLatitude = y0 + y;
+        foundLongitude = x0 + new_x;
+
+        Location copy = new Location(currentLocation);
+        copy.setLatitude(foundLatitude);
+        copy.setLongitude(foundLongitude);
+        return copy;
+    }
+
+    public static ArrayList<Location> getLocations(int amountToGenerate, long radiusInMeters) {
+
+        ArrayList<Location> locations = new ArrayList<>();
+
+        Location location = new Location("Random");
+        location.setLongitude(80.30278584);
+        location.setLatitude(27.69792155);
+
+        while (amountToGenerate-- > 0) {
+            // locations.add(getLocation(27.69792, 80.30278, 10000));
+            locations.add(getLocationInLatLngRad(radiusInMeters, location));
+        }
+
+        return locations;
+    }
+
+}
